424c1d4b22e9ea131ca496e774309cf6f7cfd6a3
c6rg0 <gdrozbik08@gmail.com>
2026-01-18 18:59:07 +0000

    Trying to update nvim init.lua

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -78,12 +78,23 @@ require('lualine').setup {
 	    statusline = 100,
 	    tabline = 100,
 	    winbar = 100,
+	    events = {
+		'WinEnter',
+		'BufEnter',
+		'BufWritePost',
+		'SessionLoadPost',
+		'FileChangedShellPost',
+		'VimResized',
+		'Filetype',
+		'CursorMoved',
+		'CursorMovedI',
+		'ModeChanged',
 	}
     },
     sections = {
 	lualine_a = {'mode'},
-	lualine_b = {'branch'},
-	lualine_c = {},
+	lualine_b = {'branch', 'diff', 'diagnostics'},
+	lualine_c = {'filename'},
 	lualine_x = {'filetype'},
 	lualine_y = {'progress'},
 	lualine_z = {'location'}
@@ -92,18 +103,28 @@ require('lualine').setup {
 	lualine_a = {},
 	lualine_b = {},
 	lualine_c = {'filename'},
-	lualine_x = {},
+	lualine_x = {'location'},
 	lualine_y = {},
 	lualine_z = {}
     },
     tabline = {
-	lualine_a = {},
-	lualine_b = {},
-	lualine_c = {},
-	lualine_x = {},
-	lualine_y = {},
-	lualine_z = {},
-    },
+	  lualine_a = {},
+	  lualine_b = {},
+	  lualine_c = {'filename'},
+	  lualine_x = {},
+	  lualine_y = {},
+	  lualine_z = {}
+	},
+
+	inactive_tabline = {
+	  lualine_a = {},
+	  lualine_b = {},
+	  lualine_c = {'filename'},
+	  lualine_x = {},
+	  lualine_y = {},
+	  lualine_z = {}
+	},
+
     winbar = {},
     inactive_winbar = {},
     extensions = {}
